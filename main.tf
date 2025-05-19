d14e6cc906a4253e861dd9b208a115dd389720cd
Attila Sovak <attila.sovak@gmail.com>
2025-05-19 09:40:59 +0200

    docs(node-problem-detector): fix description

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 /**
  * # AWS EKS node problem detector Terraform module
  *
- * A Terraform module to deploy a [node problem](https://github.com/kubernetes/node-problem-detector) detector on Amazon EKS cluster.
+ * A Terraform module to deploy a [node problem detector](https://github.com/kubernetes/node-problem-detector) on Amazon EKS cluster.
  *
  * [![Terraform validate](https://github.com/lablabs/terraform-aws-eks-node-problem-detector/actions/workflows/validate.yaml/badge.svg)](https://github.com/lablabs/terraform-aws-eks-node-problem-detector/actions/workflows/validate.yaml)
  * [![pre-commit](https://github.com/lablabs/terraform-aws-eks-node-problem-detector/workflows/pre-commit.yml/badge.svg)](https://github.com/lablabs/terraform-aws-eks-node-problem-detector/actions/workflows/pre-commit.yml)
